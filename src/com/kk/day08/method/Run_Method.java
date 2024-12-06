1dcf69e742f85375fe5a5f680a453a1eab5169bd
user1 <user1@DESKTOP-71DMN55>
2024-12-06 15:48:43 +0900

    [ADD] Day 08 클래스와 객체, 메소드, 접근지정자 및 static
    1. 클래스와 객체, 현실세계를 모델링하고 프로그래밍하기 위해서 필요한 개념.
     - 속성과 행동, 필드와 메소드, 멤버변수와 멤버메소드
     2. 메소드, 4가지 종류
      - 리턴형 X, 매개변수 X
      - 리턴형 X, 매개변수 O, 매개변수는 전달값을 받는 변수,
      - 리턴형 O, 매개변수 X, return 키워드를 반드시 적어주어야함.
      리턴값의 자료형과 리턴형은 일치해야함.
      - 리턴형 O, 매개변수 O
    3. 접근지정자 종류
     - default
     - public
     - protected
     - private
     4. static, 공유된다, 객체를 생성하지 않고도 사용가능, 이탤릭체로 표시가 됨.

---
+++ src/com/kk/day08/method/Run_Method.java
@@ -0,0 +1,24 @@
+package com.kk.day08.method;
+
+import com.kk.day08.oop.Circle;
+
+public class Run_Method {
+
+	public static void main(String[] args) {
+
+		 Exam_Method em = new Exam_Method();
+		 //리턴형 X, 매개변수 X
+		 em.noReturnNoparam();
+		 //리턴형 X, 매개변수 O
+		 em.noReturnYesParam(20250502);
+		 //리턴형 O, 매개변수 X
+		 System.out.println(em.yesReturnNoParam());
+		 //리턴형 O, 매개변수 O
+		 int result = em.yesReturnYesParam(1000);
+		 System.out.println(result);
+		 
+		 result = em.sum(1,2,3,4,5);
+		 System.out.println("result : " + result);
+	}
+	
+}
