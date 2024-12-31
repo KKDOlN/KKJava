f4f9d68c14fa1daddfa7878ab63a2a14de7d23fb
user1 <user1@DESKTOP-71DMN55>
2024-12-31 12:02:11 +0900

    [ADD] Day2x Enum의 기본개념 및 예제코드 1. 열거형 상수 Enum 기본 개념 학습 - 타입안정성 보장 - 코드 가독성 향상 - 유지보수성 향상
    2. 기본 메소드 활용
    - values, valueOf, ordinal 메소드 사용
    
    3. 추가 메소드 및 생성자 사용 가능
    - 객체처럼 사용가능함.
    - 생성자를 이용하여 열거형 상수에 설명이나 다른 값을 매핑할 수 있음.

---
+++ src/com/kk/day2x/enumex/Developer.java
@@ -0,0 +1,13 @@
+package com.kk.day2x.enumex;
+
+enum DevelType {
+	MOBILE, WEB, SERVER, FULLSTACK, FRONTEND, BACKEND
+}
+public class Developer {
+	private String name;
+	private long salary;
+	//private DeveloperType type;
+	private enum DevType {
+		MOBILE, FRONTEND, BACKEND
+	}
+}
---
+++ src/com/kk/day2x/enumex/DeveloperType.java
@@ -0,0 +1,5 @@
+package com.kk.day2x.enumex;
+
+public enum DeveloperType {
+	MOBILE, WEB, SERVER, FULLSTACK
+}
